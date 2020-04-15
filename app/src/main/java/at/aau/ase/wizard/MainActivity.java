c0fca4f12f5dde8a317480abe89f72b80cdbeb61
ankopein <ankopein@edu.aau.at>
2020-04-15 22:50:23 +0200

    [#issue24]: Send first request to server
    Kryonet in normal Java Thread. Update UI in AsyncTask. Log response
    message to logcat.

--- app/src/main/java/at/aau/ase/wizard/MainActivity.java
+++ app/src/main/java/at/aau/ase/wizard/MainActivity.java
@@ -1,12 +1,23 @@
 package at.aau.ase.wizard;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.dto.TextMessage;
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.kryonet.NetworkClientKryo;
+
 public class MainActivity extends AppCompatActivity {
     Button btnServer, btnClient;
+    String hostname = "se2-demo.aau.at";
+    NetworkClientKryo client = null;
+    TextView tv_res = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +28,51 @@ public class MainActivity extends AppCompatActivity {
         btnServer.setOnClickListener(v -> startServer());
         btnClient = findViewById(R.id.buttonClient);
         btnClient.setOnClickListener(v -> startClient());
+
+        tv_res = findViewById(R.id.textView_res);
+
+        client = new NetworkClientKryo();
+        client.registerClass(TextMessage.class);
+        client.registerCallback(basemessage -> {
+            if (basemessage instanceof TextMessage) {
+                Log.i("Received:", basemessage.toString());
+            } else {
+                Log.i("Error:", "Not a textmessage: "+basemessage.toString());
+            }
+        });
+        new NetworkThread().start();
     }
 
     private void startServer() {
-        // todo
-    }
+        new ServerRequestTask().execute("Some request");    }
 
     private void startClient() {
         // todo
+    }
+
+    class ServerRequestTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            client.sendMessage(new TextMessage("some request"));
+            return "Request send";
+        }
+
+        @Override
+        protected void onPostExecute(String taskRes) {
+            tv_res.setText(taskRes);
+        }
+    }
+
+    class NetworkThread extends Thread {
+        public void run () {
+            try {
+                client.connect(hostname);
+                Log.i("info:", "Connection to server "+hostname+" successful");
+            } catch (IOException e) {
+                Log.e("error:", "Could not connect to server "+hostname);
+                e.printStackTrace();
+            }
+        }
     }
 }
