1957eaf701f417c109c7fa207fb1a1ab8cc1d784
David Langmeier <dalangmeier@edu.aau.at>
2020-04-15 23:52:29 +0200

    add game activity, update home & lobby activity

--- app/src/main/java/at/aau/ase/wizard/LobbyActivity.java
+++ app/src/main/java/at/aau/ase/wizard/LobbyActivity.java
@@ -1,12 +1,13 @@
 package at.aau.ase.wizard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LobbyActivity extends AppCompatActivity {
-    Button btnServer, btnClient;
+    Button btnServer, btnClient, btnToGameScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,8 @@ public class LobbyActivity extends AppCompatActivity {
         btnServer.setOnClickListener(v -> startServer());
         btnClient = findViewById(R.id.lobby_btn_startClient);
         btnClient.setOnClickListener(v -> startClient());
+        btnToGameScreen = (findViewById(R.id.lobby_btn_ToGameScreen));
+        btnToGameScreen.setOnClickListener(v -> openGameActivity());
     }
 
     private void startServer() {
@@ -25,5 +28,9 @@ public class LobbyActivity extends AppCompatActivity {
 
     private void startClient() {
         // todo
+    }
+
+    private void openGameActivity() {
+        startActivity(new Intent(this, GameActivity.class));
     }
 }
