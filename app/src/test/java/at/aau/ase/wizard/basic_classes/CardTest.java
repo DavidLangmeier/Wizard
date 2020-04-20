4bafd28da591e3d377548542b70164195048cc8e
ankopein <ankopein@edu.aau.at>
2020-04-20 22:22:43 +0200

    Quick fix for the tests

--- app/src/test/java/at/aau/ase/wizard/basic_classes/CardTest.java
+++ app/src/test/java/at/aau/ase/wizard/basic_classes/CardTest.java
@@ -3,8 +3,9 @@ package at.aau.ase.wizard.basic_classes;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static at.aau.ase.wizard.basic_classes.Color.*;
-import static at.aau.ase.wizard.basic_classes.Value.*;
+import static at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Color.*;
+import static at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Value.*;
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.*;
 
 public class CardTest {
 
