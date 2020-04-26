f29b461648c47a422712eb030ddafb915d66c5f3
David Langmeier <dalangmeier@edu.aau.at>
2020-04-27 01:08:49 +0200

    add classes game & stateMessage classes, add Endscreen activity

---
+++ libnetwork/src/main/java/at/aau/ase/libnetwork/androidnetworkwrapper/networking/dto/game_objects/StateMessage.java
@@ -0,0 +1,32 @@
+package at.aau.ase.libnetwork.androidnetworkwrapper.networking.dto.game_objects;
+
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Card;
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Deck;
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Hand;
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Notepad;
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Player;
+
+public class StateMessage extends BaseMessage {
+    Player[] players;
+    Hand table;         // for the cards within 1 trickround
+    Notepad scores;
+    int roundsLeft;
+    Hand[] playerHands;
+    Card trump;
+    int dealer;
+    int activePlayer;
+    boolean gameFinished;
+
+    public StateMessage(Player[] players, Hand table, Notepad scores, int roundsLeft, Hand[] playerHands,
+                        Card trump, int dealer, int activePlayer, boolean gameFinished) {
+        this.players = players;
+        this.table = table;
+        this.scores = scores;
+        this.roundsLeft = roundsLeft;
+        this.playerHands = playerHands;
+        this.trump = trump;
+        this.dealer = dealer;
+        this.activePlayer = activePlayer;
+        this.gameFinished = gameFinished;
+    }
+}
