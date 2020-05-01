c6a8676116dd7fbd9eb84ddca0baf79aea8b9c90
ankopein <ankopein@edu.aau.at>
2020-05-01 15:08:47 +0200

    Add list of players to lobby activity
    to show all the players after they join the lobby

--- app/src/main/java/at/aau/ase/wizard/LobbyActivity.java
+++ app/src/main/java/at/aau/ase/wizard/LobbyActivity.java
@@ -2,12 +2,21 @@ package at.aau.ase.wizard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.dto.game_objects.LobbyMessage;
 import at.aau.ase.libnetwork.androidnetworkwrapper.networking.dto.game_objects.TextMessage;
+import at.aau.ase.libnetwork.androidnetworkwrapper.networking.game.basic_classes.Player;
 
 import static com.esotericsoftware.minlog.Log.*;
 
@@ -16,6 +25,11 @@ public class LobbyActivity extends AppCompatActivity {
     private Button btnToGameScreen;
     private WizardClient wizardClient = null;
     private TextView tvServerResponse = null;
+    private EditText etUsername = null;
+    private ListView lvPlayers = null;
+    private List<String> players = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +40,27 @@ public class LobbyActivity extends AppCompatActivity {
         btnServer.setOnClickListener(v -> startServer());
         btnToGameScreen = (findViewById(R.id.lobby_btn_ToGameScreen));
         btnToGameScreen.setOnClickListener(v -> openGameActivity());
-
+        btnToGameScreen.setEnabled(false);
         tvServerResponse = findViewById(R.id.lobby_text_serverResponseDisplay);
+        etUsername = findViewById(R.id.lobby_edittext_username);
+        etUsername.setOnKeyListener((v,keyCode,keyEvent) -> enteredUsername(keyCode,keyEvent));
+        lvPlayers = findViewById(R.id.lobby_list_players);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, players);
+        lvPlayers.setAdapter(arrayAdapter);
+
         wizardClient = WizardClient.getInstance();
         wizardClient.registerCallback(basemessage -> {
             String res = null;
             if (basemessage instanceof TextMessage) {
                 info("SERVER RESPONSE:"+ basemessage.toString());
                 res = ((TextMessage) basemessage).text;
-            } else {
+            }
+            else if (basemessage instanceof LobbyMessage) {
+                Player player = ((LobbyMessage) basemessage).getPlayer();
+                info("Received a LobbyMessage "+player.getName());
+                players.add(player.getName());
+            }
+            else {
                 error("Not a textmessage: "+basemessage.toString());
                 res = "Response is not a TextMessage";
             }
@@ -44,7 +70,19 @@ public class LobbyActivity extends AppCompatActivity {
             );
         });
     }
-  
+
+    private boolean enteredUsername(int keycode, KeyEvent keyevent) {
+        if (keyevent.getAction() == KeyEvent.ACTION_DOWN && keycode == KeyEvent.KEYCODE_ENTER) {
+            String username = etUsername.getText().toString();
+            wizardClient.sendMessage(new LobbyMessage(username));
+            debug(username);
+            btnToGameScreen.setEnabled(true);
+            etUsername.setEnabled(false);
+            return true;
+        }
+        return false;
+    }
+
     private void startServer() {
         wizardClient.sendMessage(new TextMessage("Some request"));
     }
