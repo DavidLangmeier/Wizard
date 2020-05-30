fb0e740520039a1150d41e0f7edfd1e92806a121
Lakitwo <lakitwo@icloud.com>
2020-05-30 13:38:19 +0200

    show ranking in endscreen

--- app/src/main/java/at/aau/ase/wizard/EndscreenActivity.java
+++ app/src/main/java/at/aau/ase/wizard/EndscreenActivity.java
@@ -1,14 +1,21 @@
 package at.aau.ase.wizard;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import at.aau.ase.libnetwork.androidnetworkwrapper.networking.dto.game_actions.Action;
 import at.aau.ase.libnetwork.androidnetworkwrapper.networking.dto.game_actions.ActionMessage;
@@ -18,15 +25,25 @@ public class EndscreenActivity extends AppCompatActivity {
     private Button btnPlayAgain;
     private Button btnExitGame;
     private TextView tvMyPlayerScore;
+    private ListView lvRanking;
 
     private WizardClient wizardClient;
     GameData gameData;
+    List<String> playersInRankingOrder;
+    private ArrayAdapter<String> arrayAdapter = null;
+    int[][] totalPointsInRankingOrder;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_endscreen);
+
+        tvMyPlayerScore = findViewById(R.id.tv_myPlayerScores);
+        btnPlayAgain = findViewById(R.id.btn_play_again);
+        btnExitGame = findViewById(R.id.btn_exit_game);
+        lvRanking = findViewById(R.id.lv_PlayerScores);
 
         wizardClient = WizardClient.getInstance();
         startCallback();
@@ -35,12 +52,21 @@ public class EndscreenActivity extends AppCompatActivity {
 
         String s2 = getIntent().getStringExtra("gameData");
         gameData = new Gson().fromJson(s2, GameData.class);
+        sortPlayersByRanking();
+        sortPlayerTotalPointsByRanking();
         //System.out.println("--------------------------------------------" + gameData.getScores().getBetTricksPerPlayerPerRound()[0][19]);
-        tvMyPlayerScore = findViewById(R.id.tv_myPlayerScores);
-        btnPlayAgain = findViewById(R.id.btn_play_again);
-        btnExitGame = findViewById(R.id.btn_exit_game);
-        String winnerText = gameData.getScores().getPlayerNamesList().get(checkWinner()) + " wins with " + Arrays.toString(gameData.getScores().getTotalPointsPerPlayer()[checkWinner()]) + " points!";
+        String winnerText = playersInRankingOrder.get(0) + " wins with " + totalPointsInRankingOrder[0][0] + " points!";
         tvMyPlayerScore.setText(winnerText);
+        Integer[] icons = new Integer[]{
+                R.drawable.rank1gold,
+                R.drawable.rank2silver,
+                R.drawable.rank3bronze,
+                R.drawable.rank0default,
+                R.drawable.rank0default,
+                R.drawable.rank0default
+        };
+        arrayAdapter = new LobbyListAdapter(this, playersInRankingOrder, icons);
+        lvRanking.setAdapter(arrayAdapter);
     }
 
     public void startCallback() {
@@ -51,13 +77,68 @@ public class EndscreenActivity extends AppCompatActivity {
         });
     }
 
-    int checkWinner() {
-        int winnerIndex = 0;
-        for (int i = 1; i < gameData.getScores().getPlayerNamesList().size(); i++) {
-            if (gameData.getScores().getTotalPointsPerPlayer()[i - 1][0] < gameData.getScores().getTotalPointsPerPlayer()[i][0]) {
-                winnerIndex = i;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    int[] rankingIndices() {
+        int[][] a = gameData.getScores().getTotalPointsPerPlayer();
+        int[] sortedIndices = IntStream.range(0, a.length)
+                .boxed().sorted((i, j) -> a[i][0] - a[j][0])
+                .mapToInt(e -> e).toArray();
+        invert(sortedIndices);
+        return sortedIndices;
+        /*int[][] a = gameData.getScores().getTotalPointsPerPlayer();
+        System.out.println("======================================== " + a.length);
+        int[] copySort = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            copySort = a[i];
+        }
+        int[] index = new int[a.length];
+        int counter = 0;
+        for (int playerIndex = 0; playerIndex < gameData.getScores().getPlayerNamesList().size(); playerIndex++) {
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a.length; j++) {
+                    if (copySort[i] == a[playerIndex][0]) {
+                        index[counter] = j;
+                        a[playerIndex][j] = -2101; //max negative points -1
+                        counter++;
+                        break;
+                    }
+                }
             }
         }
-        return winnerIndex;
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(index));
+        return index;*/
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void sortPlayersByRanking(){
+        int[] index = rankingIndices();
+        List<String> players = gameData.getScores().getPlayerNamesList();
+        List<String> returnSortedPlayers = gameData.getScores().getPlayerNamesList();
+
+        for (int i = 0; i < players.size(); i++) {
+            returnSortedPlayers.set(i, players.get(index[i]));
+        }
+        playersInRankingOrder = returnSortedPlayers;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void sortPlayerTotalPointsByRanking(){
+        int[] index = rankingIndices();
+        int[][] pPP = gameData.getScores().getTotalPointsPerPlayer();
+        int[][] returnPPP = gameData.getScores().getTotalPointsPerPlayer();
+        for (int i = 0; i < playersInRankingOrder.size(); i++) {
+            returnPPP[i][0]=pPP[index[i]][0];
+        }
+        totalPointsInRankingOrder = returnPPP;
+    }
+
+    void invert(int[]array) {
+        for (int i = 0; i < array.length/2; i++) {
+            int temp = array[i];
+            array[i]= array[array.length - 1 - i];
+            array[array.length - 1 - i]= temp;
+        }
     }
 }
---
+++ app/src/main/java/at/aau/ase/wizard/EndscreenListAdapter.java
@@ -0,0 +1,41 @@
+package at.aau.ase.wizard;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class EndscreenListAdapter extends ArrayAdapter{
+
+    private final Activity context;
+    private final Integer[] imageID;
+    private final List<String> players;
+
+    public EndscreenListAdapter(Activity context, List<String> players, Integer[] imageID){
+
+        super(context,R.layout.lobby_listview_row , players);
+
+        this.context = context;
+        this.players = players;
+        this.imageID = imageID;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.lobby_listview_row, null,false);
+
+        TextView tvName = (TextView) rowView.findViewById(R.id.etLobbyListRow);
+        ImageView ivIcon = (ImageView) rowView.findViewById(R.id.ivLobbyListRow);
+
+        tvName.setText(players.get(position));
+        ivIcon.setImageResource(imageID[position%imageID.length]);
+
+        return rowView;
+    }
+}
