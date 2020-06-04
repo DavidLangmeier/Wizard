0fbb1f5e2c35cfc1e2ee9f5ef59d3fa32010b2e4
Lakitwo <lakitwo@icloud.com>
2020-06-04 21:22:03 +0200

    remove major smell

--- app/src/main/java/at/aau/ase/wizard/EndscreenActivity.java
+++ app/src/main/java/at/aau/ase/wizard/EndscreenActivity.java
@@ -33,8 +33,8 @@ public class EndscreenActivity extends AppCompatActivity {
 
     private WizardClient wizardClient;
     GameData gameData;
-    List playersInRankingOrder = new ArrayList();
-    List playersOnline;
+    List <String> playersInRankingOrder = new ArrayList();
+    List <String> playersOnline;
     private EndscreenListAdapter arrayAdapter = null;
     int[][] totalPointsInRankingOrder;
     int[] actualIconID;
