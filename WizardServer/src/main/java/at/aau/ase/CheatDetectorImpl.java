ab5d1c5ac35a550c44b60d6112c88dc1af14cb43
ankopein <ankopein@edu.aau.at>
2020-06-05 11:58:58 +0200

    [#issue18]: Recognize jester+wizard not as cheating
    jesters and wizards can be played anytime. It is not cheating to play a
    jester/wizard if you'd need to play a certain color. They can be played
    anytime.

--- WizardServer/src/main/java/at/aau/ase/CheatDetectorImpl.java
+++ WizardServer/src/main/java/at/aau/ase/CheatDetectorImpl.java
@@ -13,36 +13,33 @@ public class CheatDetectorImpl implements CheatDetector {
 
     private Game game;
     private Hand[] playerHands;
-    //private Hand table;
-    //private Color trump;
     private List<Player> players;
     private boolean[] isCheating; // for all players
 
     public CheatDetectorImpl(Game game) {
         this.game = game;
         this.playerHands = game.getPlayerHands();
-        //this.table = game.getTable();
-        //this.trump = game.getTrump();
         this.players = game.getPlayers();
         this.isCheating = new boolean[this.players.size()];
     }
 
     public void update(Card playedCard) {
-        int activePlayerIdx = playedCard.getPlayedBy(); //game.getActivePlayerIndex();
+        int activePlayerIdx = playedCard.getPlayedBy();
         Hand handToCheck = playerHands[activePlayerIdx];
         Color activeColor = game.getActiveColor();
 
         if (playedCard.getColor() != activeColor) { // Possibly cheating, check players cardsInHand
-            for (Card cardInHand : handToCheck.getCards()) {
-                if (cardInHand.getColor() == activeColor) { // Cheating!
-                    isCheating[activePlayerIdx] = true;
-                    break;
+            if (playedCard.getColor() != Color.JESTER && playedCard.getColor() != Color.WIZARD) { // Jester + Wizard can always be played
+                for (Card cardInHand : handToCheck.getCards()) {
+                    if (cardInHand.getColor() == activeColor) { // Cheating!
+                        isCheating[activePlayerIdx] = true;
+                        break;
+                    }
                 }
             }
         }
-        for (boolean b: isCheating) {
-            Log.info("ischeating = "+b);
-            Log.info("activeplayeridx = "+ (activePlayerIdx));
+        for (int i = 0; i < isCheating.length; i++) {
+            Log.info("=============> CHEATING: ischeating("+players.get(i).getName()+") = "+isCheating[i]);
         }
     }
 
